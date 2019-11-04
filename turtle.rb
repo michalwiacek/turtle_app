0b8e3f5c8712ae8903449d23b106b0716f775b0c
michal <thepowerofra@gmail.com>
2019-11-04 23:51:20 +0100

    implement basic user interface

--- turtle.rb
+++ turtle.rb
@@ -43,11 +43,53 @@ class Turtle
       when "SOUTH"
         @y+=1 if @y < @table[1]
       when "EAST"
-        @x+=1 if @x < table[0]
+        @x+=1 if @x < @table[0]
       when "WEST"
         @x-=1 if @x >0
       when "NORTH"
         @y-=1 if @y > 0
+    end
+  end
+end
+
+class UserInterface
+prompt = "> "
+print prompt
+@turtle = Turtle.new [4, 4]
+def self.place_turtle(x, y, d)
+  @turtle.place x, y, d
+end
+
+while ui = gets.chomp
+  user_input = []
+  ui.gsub!(",", " ")
+  user_input = ui.split(" ")
+  puts user_input[0]
+  case user_input[0]
+    when "PLACE"
+      if user_input.length == 4
+        place_turtle x=user_input[1].to_i, y=user_input[2].to_i, d=user_input[3]
+      else
+        puts "We need exacly 3 arguments"
+      end
+      print prompt
+    when "MOVE"
+      @turtle.walk
+      print prompt
+    when "RIGHT"
+      @turtle.turn_right
+      print prompt
+    when "LEFT"
+      @turtle.turn_left
+      print prompt
+    when "REPORT"
+      puts @turtle.report
+      print prompt
+    when "EXIT"
+      break
+    else
+      puts "sorry, wrong method, try EXIT"
+      print prompt
     end
   end
 end
